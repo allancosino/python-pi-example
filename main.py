c146ccb1348568d25eede6d0d2f03f9fff0ae7a6
Allan <allancosino@live.ca>
2020-06-11 06:42:37 -0500

    merged conflicts resolved

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 
 def main(msg):
-<<<<<<< HEAD
 	// Adding the second comment to this file.
-=======
 	// Print a message
->>>>>>> refs/remotes/orgin/master
     print(msg)
 
 main("Hello people")
